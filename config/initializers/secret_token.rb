100c69a1b24472c6e03e79a988dc6b2d378b9dc6
Clearwire-MBP <clearwire-mbp@Clearwire-MBPs-MacBook-Pro.local>
2011-05-29 16:38:39 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'c737c6575af5bf1142c6fbd3fce386595628e0c3787644fff0db710bede89c564c81c231e72f58d981df6c493987aa63e1af8ea90df927ff1e39dcd681857834'
